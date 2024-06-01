6646eb2f2969644572bc7cb9fe0cb5c0c4740fec
Liam263 <nguyenthanhlam091@gmail.com>
2024-06-01 13:44:44 +1000

    update

--- api/index.js
+++ api/index.js
@@ -145,47 +145,7 @@ app.get("/getData", async (req, res) => {
           account_status: account.Status,
         })),
         { updateOnDuplicate: ["account_id", "entity_id"], transaction: t }
-      ),
-      db.BankTransactions.bulkCreate(
-        bankTransactions.map(transaction => ({
-          entity_id: ENTITY_ID,
-          transaction_id: transaction.BankTransactionID,
-          transaction_status: transaction.Status,
-          contact_id: transaction.Contact ? transaction.Contact.ContactID : null,
-          contact_name: transaction.Contact ? transaction.Contact.Name : null,
-          transaction_date: parseXeroTimestamp(transaction.Date),
-          bank_account_id: transaction.BankAccount
-            ? transaction.BankAccount.AccountID
-            : null,
-          account_code: transaction.BankAccount
-            ? transaction.BankAccount.Code
-            : null,
-          bank_account_name: transaction.BankAccount
-            ? transaction.BankAccount.Name
-            : null,
-          transaction_currency: transaction.CurrencyCode,
-          currency_rate: transaction.CurrencyRate
-            ? transaction.CurrencyRate
-            : null,
-          transaction_type: transaction.Type,
-          item_ID: transaction.LineItems
-            ? transaction.LineItems.LineItemID
-            : null,
-          item_description: transaction.LineItems
-            ? transaction.LineItems.Description
-            : null,
-          item_quantity: transaction.LineItems
-            ? transaction.LineItems.Quantity
-            : null,
-          item_unit_price: transaction.LineItems
-            ? transaction.LineItems.UnitAmount
-            : null,
-          sub_total: transaction.SubTotal,
-          total_tax: transaction.TotalTax,
-          total_amount: transaction.Total,
-        })),
-        { updateOnDuplicate: ["entity_id", "transaction_id"], transaction: t }
-      ),
+      )
     ]);
 
     await t.commit();
